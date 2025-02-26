352d032ecc498d06c2621d1f4a7bf6414247abad
Bjoern <beck.b.hh@gmail.com>
2025-02-26 12:44:18 +0100

    update async function. implement dynamic cards. add a default value for the type, if the value type is empty.

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
+const urlApi = "https://rickandmortyapi.com/api/character";
+
 // States
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
-// Fetch the data from the API 
-async function fetchCharacters(page) {
+// Fetch the data from the API and store the data in const arrayCharater
+async function fetchCharacters(urlApi) {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const response = await fetch(urlApi);
     const contentType = response.headers.get("content-type");
 
     if (!response.ok) {
@@ -30,7 +32,17 @@ async function fetchCharacters(page) {
       throw new Error(`Uups, das ist kein json Format ${contentType} `);
     }
 
-    return await response.json();
+    // Get all the characters in the array 
+    const apiData = await response.json()
+    const arrayCharater = apiData.results;
+
+    // Emptied every time the cardContainer before fetch new characters
+    cardContainer.innerHTML = "";
+
+    // Get one by one character from the hole big array (arrayCharater)
+    arrayCharater.forEach((character) => {
+      cardContainer.append(CharacterCard(character));
+    });
 
   } catch (error) {
     console.log(`Fehler ist geworfen: ${error.message} `);
@@ -38,7 +50,4 @@ async function fetchCharacters(page) {
   }
 }
 
-// Create a new Character Card via function
-cardContainer.append(CharacterCard());
-
-console.log(fetchCharacters());
+fetchCharacters(urlApi);
